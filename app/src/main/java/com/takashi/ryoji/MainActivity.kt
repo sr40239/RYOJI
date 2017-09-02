696801ea886950626a13aeaf2f068e8c6ad65a02
sr40239 <sr40239.gmail.com>
2017-09-02 22:30:25 +0900

    add TweetClass

--- app/src/main/java/com/takashi/ryoji/MainActivity.kt
+++ app/src/main/java/com/takashi/ryoji/MainActivity.kt
@@ -2,14 +2,16 @@ package com.takashi.ryoji
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
 
-        // send message to RYOJI-SAN
+        // authorication
+      //  val btn : (Button) = findViewById(R.id.request);
     }
 }
---
+++ app/src/main/java/com/takashi/ryoji/Tweet.kt
@@ -0,0 +1,20 @@
+package com.takashi.ryoji
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by r_s on 2017/09/02.
+ */
+
+class Tweet : AppCompatActivity() {
+
+    //
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+
+        // authorication
+        //  val btn : (Button) = findViewById(R.id.request);
+    }
+}
